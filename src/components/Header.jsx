f9782583d0c6e21cc4d8d1d83820dbb942ab79e2
ThiagoTerraDev <thiagoterradev@gmail.com>
2023-11-24 13:49:24 -0300

    Criação e estilização do Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React from "react";
+import styles from "./Header.module.css";
+import { NavLink } from "react-router-dom";
+import { ReactComponent as Logo } from "../assets/logo.svg";
+
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className={styles.header}>
+      <div className="container1">
+        <nav className={styles.navigation}>
+          <NavLink to="/" end>Home</NavLink>
+          <NavLink to="/sobre">Sobre</NavLink>
+          <NavLink to="/" end aria-label="South Journey - Home">
+            <Logo />
+          </NavLink>
+          <NavLink to="/termosecondicoes">Termos e Condições</NavLink>
+          <NavLink to="/contato">Contato</NavLink>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
